9a07909553fe7eb07d6cd13992af5edcfc7044dc
Brad Bryant <bradleybryant86@gmail.com>
2020-12-15 00:34:38 -0600

    Wrote loadCharacters function to request info from hp API

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-const charactersList = document.getElementById("charactersList")
+const charactersList = document.getElementById("charactersList");
+
+const loadCharacters = async () => {
+    try {
+        const res = await fetch('http://hp-api.herokuapp.com/api/characters');
+        let hpCharacters = await res.json();
+        displayCharacters(hpCharacters);
+        console.log(hpCharacters);
+    } catch (err) {
+        console.err(err);
+    }
+}
