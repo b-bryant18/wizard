ca5f4fe9acd712ddaf02cc7abdc13b0b9e1ea410
Brad Bryant <bradleybryant86@gmail.com>
2020-12-15 00:40:10 -0600

    Wrote displayCharacters function with JSX and called loadCharacters function

--- app.js
+++ app.js
@@ -9,4 +9,21 @@ const loadCharacters = async () => {
     } catch (err) {
         console.err(err);
     }
-}
+};
+
+const displayCharacters = (characters) => {
+    const htmltring = characters
+        .map((character) => {
+            return `
+    <li class= "character">
+    <h2>${character.name}</h2>
+    <p>House: ${character.house}</p>
+    <img src="${character.image}"</img>
+    </li>
+    `;
+        })
+        .join('');
+    charactersList.innerHTML = htmltring;
+};
+
+loadCharacters();
